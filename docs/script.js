e3bce65300fb63b96c9545a76fc2e6f373e69138
Deekshith Muthoju <muthojudeekshith@gmail.com>
2026-04-11 03:11:14 +0530

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,6 @@
 /* CONFIG */
 const UNLOCK_DATE = new Date('2026-01-10T00:00:00');
-const PASSWORD = 'c5d9';
+const PASSWORD = 'foreverCC';
 let unlocked = false;
 
 const screens = document.querySelectorAll('.screen');
